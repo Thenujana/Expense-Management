115ef482ff4e272df1f06eb8dbdffd3ac0bb3603
Thenujana <chenarathenujana@gmail.com>
2026-04-03 12:46:43 +0530

    changes in controller class cross origin

--- src/main/java/edu/icet/controller/ExpenseController.java
+++ src/main/java/edu/icet/controller/ExpenseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/expense")
 @RequiredArgsConstructor
 public class ExpenseController {
